79b9e27cfb13f569cc6e43afbdff0fe0faef6428
Julian Vos <post@julianvos.nl>
2024-01-28 21:49:48 +0100

    chore: add success and fail activity start examples

---
+++ display-success.sh
@@ -0,0 +1,3 @@
+#!/bin/env bash
+
+adb -s "$1" shell am start -n nl.paytree.status_display/.SuccessActivity
--- example.sh
+++ example.sh
@@ -4,4 +4,4 @@ date
 echo
 
 model=$(adb -s "$1" shell getprop ro.product.model)
-echo "Current device: $model ($1)"
+notify-send "Current device: $model ($1)"
